b84f2ce5e70b69ae437a7abefb940041a8559295
Fernando Thomé <fe_thome@hotmail.com>
2021-02-26 09:57:37 -0300

    nlw4-react-day5

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,9 @@
 import '../styles/global.css';
-import { ChallengesProvider } from "../contexts/ChallengesContext";
-import { CoutdownProvider } from '../contexts/CountdownContext';
-
 
 function MyApp({ Component, pageProps }) {
 
   return (
-    <ChallengesProvider>
-     
         <Component {...pageProps} />
-      
-    </ChallengesProvider>
-  
   )
 }
 
